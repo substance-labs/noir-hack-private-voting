ce4f7d88ba18e70e5d7fd392d828f56e3da730c9
Alessandro Manfredi <alle.manfredi@gmail.com>
2025-05-08 20:23:05 +0200

    refactor(ui): reads urls from process.env

--- ui/src/settings/index.js
+++ ui/src/settings/index.js
@@ -2,6 +2,6 @@ export default {
   addresses: {
     revelio: "0x6a39439968FBe0d5E8996b9643D3bbd58dc58141",
   },
-  apiUrl: "http://localhost:3000",
-  ipfsGateway: "https://ipfs.io/ipfs",
+  apiUrl: process.env.RELAYER_URL,
+  ipfsGateway: process.env.IPFS_GATEWAY,
 }
